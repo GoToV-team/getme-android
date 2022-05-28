464a9694cc9d80739110a55244a33a8b6db2fc1d
TheCompiler <vet_v2002@mail,ru>
2022-05-28 21:27:42 +0300

    try fix klint

--- app/src/main/java/com/gotov/getmeapp/app/di/OkhttpInterseptor.kt
+++ app/src/main/java/com/gotov/getmeapp/app/di/OkhttpInterseptor.kt
@@ -1,10 +1,10 @@
 package com.gotov.getmeapp.app.di
 
 import android.util.Log
+import java.io.IOException
 import okhttp3.Interceptor
 import okhttp3.Request
 import okhttp3.Response
-import java.io.IOException
 import okio.Buffer
 
 object CookiesStore {
